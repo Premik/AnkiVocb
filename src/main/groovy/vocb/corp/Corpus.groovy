ce442bc264299f00ef5d74240bef0cb8f2ca36bb
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-04-11 23:55:02 +0200

    Freq

--- src/main/groovy/vocb/corp/Corpus.groovy
+++ src/main/groovy/vocb/corp/Corpus.groovy
@@ -9,12 +9,14 @@ import com.xlson.groovycsv.CsvParser
 
 import groovy.xml.XmlSlurper
 import groovy.xml.slurpersupport.GPathResult
+import vocb.ConfHelper
+import vocb.Helper
 
 
 public class Corpus {
 
 
-	Map<String, Double> wordFreq = new HashMap(5000)
+	Map<String, BigDecimal> wordFreq = new HashMap(5000)
 	Set<String> phrases = new HashSet<String>(2000)
 
 
@@ -23,9 +25,15 @@ public class Corpus {
 		String dispersionCol="Dispersion"
 		Iterator csvLines = CsvParser.parseCsv(parserArgs, reader)
 		for (line in csvLines) {
-			wordFreq[line.Word.toLowerCase()] = line."Dispersion"
+			String w = line."Word".toLowerCase().replace('\u00A0',' ').trim()
+			String f = line."Frequency"
+			//String f  = line."Dispersion"
+			if (!f || !w) {
+				continue
+			}
+			wordFreq[w] = new BigDecimal(f)
 		}
-		println "Imported ${wordFreq.size()} words"
+		println "Imported ${wordFreq.size()} words from Csv"
 	}
 
 	//https://github.com/en-wl/wordlist/tree/master/alt12dicts
@@ -48,7 +56,7 @@ public class Corpus {
 			//println w
 
 			if (limit > 1 || limit==0) {
-				wordFreq[w.toLowerCase()] = new Double(1-groupIndex/120d) //Magic factor to get similar Disp as wordfreq
+				wordFreq[w.toLowerCase()] = new BigDecimal(1-groupIndex/120d) //Magic factor to get similar Disp as wordfreq
 				if (limit != 0) {
 					limit--
 				}
@@ -76,9 +84,11 @@ public class Corpus {
 	//https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/PG/2006/04/1-10000
 
 	void importWikiWordList(InputStream input, int limit = 0) {
+		assert input != null : "No input provided"
 		//SAXParser parser = new SAXParser()
 		//def parser =new org.ccil.cowan.tagsoup.Parser()
 		//def page = new XmlSlurper(parser).parse(input)
+
 		def page = new XmlSlurper().parse(input)
 		GPathResult content = page.body.'**'.find {it.@id == 'bodyContent'}
 		content.'**'.each { GPathResult table ->
@@ -86,35 +96,73 @@ public class Corpus {
 
 			table.tbody.tr.eachWithIndex { GPathResult row, int i ->
 				if (i == 0) {return}
-				String w = row.td[1].a.toString().trim()
-				String r = row.td[2].toString().trim()
+				String w = row.td[1].a.toString().trim().toLowerCase()
+				String r = row.td[2].toString().trim().toLowerCase()
 
-				wordFreq[w.toLowerCase()] = new Double(r)
+				wordFreq[w.toLowerCase()] = new BigDecimal(r)
 			}
 		}
-		println "Imported ${wordFreq.size()} words."
+		println "Imported ${wordFreq.size()} words for wiki pages"
 
 
 	}
 
-	void importCsvCorpus(Map parserArgs = [:], InputStream str) {
+	public void importCsvCorpus(Map parserArgs = [:], InputStream str) {
 		importCsvCorpus(parserArgs, new InputStreamReader(str, StandardCharsets.UTF_8))
 	}
 
-	void loadInbuild() {
-		//import12WordFreq(getClass().getResource('/2+2+3frq.txt').openStream())
-		importWikiWordList(Corpus.class.getResource('/Wiktionary_Frequency lists_PG_2006_04_1-10000.html').openStream())
+	void loadWiki() {
+		importWikiWordList(ConfHelper.instance.resolveRes("Wiktionary_Frequency lists_PG_2006_04_1-10000 - Wiktionary"))
+		importWikiWordList(ConfHelper.instance.resolveRes("Wiktionary Frequency lists_PG_2006_04_10001-20000 - Wiktionary"))
+		importWikiWordList(ConfHelper.instance.resolveRes("Wiktionary Frequency lists_PG_2006_04_20001-30000 - Wiktionary"))
+		importWikiWordList(ConfHelper.instance.resolveRes("Wiktionary Frequency lists_PG_2006_04_30001-40000 - Wiktionary"))
+	}
+
+	void load12Dicts() {
 		import12Phrases(getClass().getResource('/6of12.txt').openStream())
+		import12WordFreq(getClass().getResource('/2+2+3frq.txt').openStream())
+	}
+
+	void loadWordFreq() {
+		importCsvCorpus(getClass().getResource('/wordFreq.csv').openStream())
+	}
+
+
+
+	void averageCommonWordsFrom(Corpus c) {
+		assert c?.wordFreq : "No corpus or blank freq. words provided"
+		Map<String, BigDecimal> f1 = wordFreq
+		Map<String, BigDecimal> f2 = c.wordFreq
+
+		Set<String> commonWords = f1.keySet().intersect(f2.keySet())
+		/*commonWords.take(100).each { String w->
+		 println "$w:  ${f1[w]/f2[w]} ( ${f1[w]-f2[w]})  ${f1[w]} ${f2[w]} }"
+		 }*/
+
+		commonWords.each { String w->
+			f1[w] = (f1[w] + f2[w]) /2
+			
+		}
+	}
+
+	static Corpus buildDef() {
+		Corpus c1 = new Corpus()
+		c1.loadWiki()
+		Corpus c2 = new Corpus()
+		//c2.load12Dicts()
+		c2.loadWordFreq()
+		c1.averageCommonWordsFrom(c2)
+		return c1
+
 	}
 
 	static void main(String... args) {
 
-		Corpus n = new Corpus()
+		Corpus c= buildDef()
+
 		//n.importCsvCorpus(getClass().getResource('/wordFreq.csv').openStream())
 		//n.import12WordFreq(getClass().getResource('/2+2+3frq.txt').openStream())
 		//n.importWikiWordList(Corpus.class.getResource('/Wiktionary_Frequency lists_PG_2006_04_1-10000.html').openStream())
-
-		n.loadInbuild()
 
 
 	}
