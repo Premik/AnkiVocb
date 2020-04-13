c0b5cfa6d08fab741da3f7ecc99a76301a51d01d
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-04-13 23:31:33 +0200

    Cs trn

---
+++ src/main/groovy/vocb/appenders/TranslationAppender.groovy
@@ -0,0 +1,40 @@
+package vocb.appenders
+
+import vocb.HttpHelper
+import vocb.azure.AzureTranslate
+import vocb.data.Concept
+import vocb.data.Manager
+import vocb.data.Term
+
+public class TranslationAppender {
+
+
+	AzureTranslate trn = new AzureTranslate(httpHelper: new HttpHelper() )
+
+	Manager dbMan = new Manager()
+
+	void run() {
+
+		dbMan.load()
+		List<Concept> noCs = dbMan.db.concepts.findAll {
+			it.terms && it.state!="ignore" && (!it.termsByLang("cs"))
+		}
+
+		for (Concept c in noCs) {
+			Map trnJson = trn.trnJsonrunTrn(c.firstTerm)
+			trn.extractTopTrns(trnJson).each {String csWord ->
+				c.terms.put(csWord, new Term(csWord, "cs"))
+			}
+			dbMan.save()
+			//break
+		}
+	}
+
+
+	public static void main(String[] args) {
+		TranslationAppender a = new TranslationAppender()
+		a.run()
+
+		println "Done"
+	}
+}
