f29e1c4c3872095d8e3d0be8d24697bb565106e1
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-04-18 16:02:46 +0200

    on off quotes

--- src/main/groovy/vocb/data/ConceptYamlStorage.groovy
+++ src/main/groovy/vocb/data/ConceptYamlStorage.groovy
@@ -47,7 +47,7 @@ public class ConceptYamlStorage {
 
 	public String yamlHash(String key, String value) {
 		if (!key || !value) return ""
-		boolean quote = value in ['true', 'false', "Yes", "No", "yes", "no"]
+		boolean quote = value in ['true', 'false', "Yes", "No", "yes", "no", "off", "on"]
 		if (quote) { return """$key: "$value"\n"""}
 		return "$key: $value\n"
 	}
