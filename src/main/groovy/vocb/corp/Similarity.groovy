87bf0f12dd23779a6544da91cfeb32b0fa1b190c
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-04-21 15:13:49 +0200

    Siml

---
+++ src/main/groovy/vocb/corp/Similarity.groovy
@@ -0,0 +1,76 @@
+package vocb.corp
+
+import java.util.concurrent.CountDownLatch
+import java.util.function.Function
+import java.util.regex.Pattern
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
+
+public class Similarity {
+
+	//https://stackoverflow.com/questions/10433657/how-to-determine-character-similarity
+	List<String> visualLettersSim= [
+		'sbgae',
+		'rp',
+		'do',
+		'bhd',
+		'mw',
+		'il',
+		'vx'
+	]
+
+	double sameLatter(char a, char b,  double exactScore=1, double visualScore=0.01) {
+		if (a==b) return exactScore
+	}
+
+	List<String> allSubstringsWithLen(CharSequence s, int len=s.length()) {
+		int count = s.length() - len +1
+		if (count <=0) return []
+		return (0..count-1).collect { s[it..it+len-1] }
+	}
+
+	List<String> allSubstringsOf(CharSequence s) {
+		(s.length()..1).collect {allSubstringsWithLen(s, it)}.collectMany {it}
+	}
+
+	List<Integer> distanceMatches(List<String> aLst, List<String> bLst, int maxDist=-1) {
+		aLst.withIndex().collect { String a, int ai ->
+			int bi = bLst.indexOf(a)
+			if (bi <0) return maxDist
+			return Math.abs(bi-ai)
+		}
+
+	}
+
+	BigDecimal similarWithLen(CharSequence a, CharSequence b, int len) {
+		List<String> aSubs = allSubstringsWithLen(a, len)
+		List<String> bSubs = allSubstringsWithLen(b, len)		
+		List<Integer> dst = distanceMatches(aSubs, bSubs, 1000) + distanceMatches(bSubs, aSubs, 1000)
+		dst.collect{ (it+1) as BigDecimal}.collect {1/(it*it)}.collect {(it < 0.01) ? 0 : it }.sum()		
+	}
+	
+	BigDecimal similarSubstrings(CharSequence a, CharSequence b) {
+		assert a
+		assert b
+		int len = Math.min(a.length() ,b.length())
+		(len..1).collect {similarWithLen(a, b, it)*it }.sum()
+	}
+
+	Set<String> commonSubstringsOf(CharSequence a, CharSequence b) {
+		Set<String> aSet = allSubstringsOf(a)
+		Set<String> bSet = allSubstringsOf(b)
+		return aSet.intersect(bSet)
+	}
+
+	static void main(String... args) {
+		Similarity n = new Similarity()
+		Corpus c=  Corpus.buildDef()
+		
+		//bedroom
+		
+
+	}
+
+
+}
