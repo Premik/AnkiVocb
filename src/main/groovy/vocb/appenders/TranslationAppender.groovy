1f96dcaa02da92f9f71a4124d544ec4b91d3aab9
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-05-19 22:21:34 +0200

    Examples

--- src/main/groovy/vocb/appenders/TranslationAppender.groovy
+++ src/main/groovy/vocb/appenders/TranslationAppender.groovy
@@ -11,6 +11,7 @@ public class TranslationAppender {
 
 	AzureTranslate trn = new AzureTranslate(httpHelper: new HttpHelper() )
 	int sleep =500
+	int limit= 100
 
 	Manager dbMan = new Manager()
 
@@ -20,41 +21,53 @@ public class TranslationAppender {
 		List<Concept> noCs = dbMan.db.concepts.findAll {
 			it.terms && it.state!="ignore" && (!it.termsByLang("cs"))
 		}
-
+		int i = 0
 		for (Concept c in noCs) {
 			Map trnJson = trn.trnJsonrunTrn(c.firstTerm)
 			trn.extractTopTrns(trnJson).each {String csWord ->
 				c.terms.put(csWord, new Term(csWord, "cs"))
 			}
 			dbMan.save()
+			if (i>limit) break
+				i++
 			Thread.sleep(sleep)
 			//break
 		}
 	}
-	
+
 	void translateExamples() {
-		
-				dbMan.load()
-				List<Concept> noCs = dbMan.db.concepts.findAll {
-					it.terms && it.state!="ignore" &&  (!it.examples) && it.firstTerm)
-				}
-		
-				for (Concept c in noCs) {
-					Map trnJson = trn.trnJsonrunTrn(c.firstTerm)
-					trn.extractTopTrns(trnJson).each {String csWord ->
-						c.terms.put(csWord, new Term(csWord, "cs"))
-					}
-					dbMan.save()
-					Thread.sleep(sleep)
-					//break
-				}
+
+		dbMan.load()
+		List<Concept> noCs = dbMan.db.concepts.findAll {
+			it.terms && it.state!="ignore" && it.examplesByLang("en") && it.firstTerm && (!it.examplesByLang("cs"))  
+		}
+		int i = 0
+		for (Concept c in noCs) {
+			String enSam = c.examplesByLang("en")[0]
+			Map trnJson = trn.trnJsonrunTrn(enSam)
+			List<String> csSams =trn.extractTopTrns(trnJson)
+			println "$c.firstTerm $enSam $csSams"
+			csSams.each {String csTrn ->
+				
+				c.examples.put(csTrn, new Term(csTrn, "cs"))
+				
 			}
+			dbMan.save()
+			if (i>limit) break
+				i++
+			Thread.sleep(sleep)
+			//break
+		}
+	}
 
 
 	public static void main(String[] args) {
-		TranslationAppender a = new TranslationAppender()
-		a.translateWords()
-		a.translateExamples()
+		new TranslationAppender().with {
+		
+		limit = 1
+		//a.translateWords()
+		translateExamples()
+		}
 
 		println "Done"
 	}
