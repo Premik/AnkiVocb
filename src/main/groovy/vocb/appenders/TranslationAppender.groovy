18a82536b3b2a515c3b28aba043ff809710105d2
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-05-19 23:26:07 +0200

    Samples

--- src/main/groovy/vocb/appenders/TranslationAppender.groovy
+++ src/main/groovy/vocb/appenders/TranslationAppender.groovy
@@ -12,11 +12,18 @@ public class TranslationAppender {
 
 	AzureTranslate trn = new AzureTranslate(httpHelper: new HttpHelper() )
 	AwsTranslate awsTrn = new AwsTranslate()
-	
+
 	int sleep =500
 	int limit= 100
 
 	Manager dbMan = new Manager()
+
+
+	List<Concept> findTodo() {
+		dbMan.db.concepts.findAll {
+			it.terms && it.state!="ignore" && it.examplesByLang("en") && it.firstTerm && (!it.examplesByLang("cs"))
+		}
+	}
 
 	void translateWords() {
 
@@ -41,37 +48,61 @@ public class TranslationAppender {
 	void translateExamples() {
 
 		dbMan.load()
-		List<Concept> noCs = dbMan.db.concepts.findAll {
-			it.terms && it.state!="ignore" && it.examplesByLang("en") && it.firstTerm && (!it.examplesByLang("cs"))  
-		}
+		List<Concept> noCs = findTodo()
 		int i = 0
 		println "Found ${noCs.size()} concept with missing cs trn"
 		for (Concept c in noCs) {
 			String enSam = c.examplesByLang("en")[0]?.term
 			String[] csSams = [awsTrn.trn(enSam)]
-			//Map trnJson = trn.dictLookup(enSam)			
+			//Map trnJson = trn.dictLookup(enSam)
 			//List<String> csSams =trn.extractTopTrns(trnJson)
 			println "$c.firstTerm $enSam $csSams"
 			csSams.each {String csTrn ->
-				
 				c.examples.put(csTrn, new Term(csTrn, "cs"))
-				
 			}
+
 			dbMan.save()
-			if (i>limit) break
-				i++
+			if (i>=limit) {
+				break
+			}
+			i++
 			Thread.sleep(sleep)
 			//break
 		}
 	}
 
+	void reuseTranlation() {
+		dbMan.load()
+		Map<String, Concept> byEnSample = dbMan.db.concepts
+				.findAll {it.examplesByLang("cs")}
+				.collectEntries { Concept c->
+					[c.examplesByLang("en")[0]?.term, c]
+				}
+				
+				
+
+		for (Concept c in findTodo()) {
+			String enSample = c.examplesByLang("en")[0]?.term
+			if (!enSample) continue
+			String csSample = byEnSample[enSample]?.examplesByLang("cs")?.first()?.term
+			if (!csSample) continue
+				println "'$c.firstTerm' Reusing: $csSample"
+			c.examples.put(csSample, new Term(csSample, "cs"))
+
+		}
+		dbMan.save()
+	}
+
+
 
 	public static void main(String[] args) {
 		new TranslationAppender().with {
-		
-		limit = 1
-		//a.translateWords()
-		translateExamples()
+
+			limit = 1
+			//a.translateWords()
+			reuseTranlation()
+			translateExamples()
+
 		}
 
 		println "Done"
