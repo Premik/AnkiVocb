43ff53b6b5f177cb31d51bb1987b024c536ecb56
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-05-21 10:06:03 +0200

    Folders

--- src/test/groovy/vocb/anki/crowd/Data2CrowdTest.groovy
+++ src/test/groovy/vocb/anki/crowd/Data2CrowdTest.groovy
@@ -5,23 +5,15 @@ import org.junit.jupiter.api.Test
 import vocb.data.Concept
 import vocb.data.ConceptDb
 import vocb.data.ConceptYamlStorage
+import vocb.data.Manager
 
 class Data2CrowdTest {
 
 	Data2Crowd dc = new Data2Crowd()
 	URL testConceptsUrl = getClass().getResource('/vocb/data/fullyPopulated.yaml')
-
-	@Test
-	void testNumberOfStars() {
-		assert dc.numberOfStarts(0) ==null
-		assert dc.numberOfStarts(10) == 0
-		assert dc.numberOfStarts(16*1000) == 1
-		assert dc.numberOfStarts(600*1000) == 2
-		assert dc.numberOfStarts(1600*1000) == 3
-
-		assert dc.numberOfStarts(null) == null
-	}
 	
+
+		
 	@Lazy Concept firstConcept= {
 		ConceptDb db =  new ConceptYamlStorage().parseDb(testConceptsUrl.newReader())
 		db.concepts[0]		
