e71fb2cd579563b857e9aaa09235fcf4e47ccff6
Premysl Srubar <premysl.srubar@leaseplan.com>
2020-07-25 12:23:31 +0200

    More imgs

--- src/main/groovy/vocb/pck/Pack.groovy
+++ src/main/groovy/vocb/pck/Pack.groovy
@@ -207,9 +207,10 @@ public class Pack {
 			//info.name = "FiveLittleMonkeys"
 			//info.name = "LondonBridge"
 			//info.name = "MaryHadALittleLamb"
+			//info.displayName = "Mary Had a Little Lamb"
 			info.name = "MyBonnie"
 			
-			//info.displayName = "Mary Had a Little Lamb"
+			
 			cfgHelper.lookupFoldersToConsider.add(packagePath.toString())
 		 
 			
