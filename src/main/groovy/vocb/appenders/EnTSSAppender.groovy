c538f61a956d146dc8f4e9f8675391eb6ad047f8
Premik <premysl.srubar@post.cz>
2025-05-24 09:51:51 +0200

    En tts

--- src/main/groovy/vocb/appenders/EnTSSAppender.groovy
+++ src/main/groovy/vocb/appenders/EnTSSAppender.groovy
@@ -10,47 +10,70 @@ import vocb.data.Term
 import vocb.tts.AwsCliPollyTTS
 
 public class EnTSSAppender {
-	
-	String[] voices = ["Emma", "Amy", "Brian", "Joanna", "Matthew"]
+
+	String[] voices = [
+		"Emma",
+		"Amy",
+		"Brian",
+		"Joanna",
+		"Matthew"
+	]
 	int voiceCounter = 0
+	int limit = 5
+	int sleep = 500
+
 	String getSomeVoice() {
 		voiceCounter++
 		voices[voiceCounter%(voices.length)]
-	} 
+	}
 
 	AwsCliPollyTTS enTts = new AwsCliPollyTTS()
 	Manager dbMan = new Manager()
-
+	int i =0
 	void runTerms() {
 		dbMan.load()
-		
+
+
 		dbMan.withTermsByLang("en") {Concept c, Term t->
+			if (c.terms.size() >3 || c.examples.size()>2) {
+				println "Ignoring: $c"
+				return
+			}
 			if (!dbMan.linkedMediaExists(t.tts) ) {
+				i++
+				if (i > limit) {					
+					return
+				}
 				t.tts = dbMan.resolveMedia(t.term, "mp3") { Path path ->
 					Process p=  enTts.synth(t.term, "neural", someVoice, "text", path.toString())
 					Helper.printProcOut(p)
 					p.waitFor(5, TimeUnit.SECONDS)
 					dbMan.save()
+					Thread.sleep(sleep)
 				}
 			}
 		}
 	}
-	
+
 	void runExamples() {
 		dbMan.load()
 		List<Concept> noEx = dbMan.db.concepts.findAll {
-			it.terms && it.state!="ignore" && it.firstTerm && it.examples 
+			it.terms && it.state!="ignore" && it.firstTerm && it.examples
 		}
 
 		int i =0
 		for (Concept c in noEx) {
-			
+			if (c.terms.size() >3 || c.examples.size()>2) {
+				println "Ignoring: $c"
+				continue
+			}
+
 			Term enSample = c.examples.values()[0]
-			if (enSample.lang != 'en' || enSample.tts) continue
-			i++
-			if (i > 5) break
-			String enWord = c.firstTerm
-			
+			if (enSample.lang != 'en' || enSample.tts) { continue}
+				i++
+			if (i > limit) break
+				String enWord = c.firstTerm
+
 			String tts = enTts.SSMLEmphSubstr(enSample.term, enWord )
 			if (!dbMan.linkedMediaExists(enSample.term) ) {
 				enSample.tts = dbMan.resolveMedia(enSample.term, "mp3") { Path path ->
@@ -58,20 +81,17 @@ public class EnTSSAppender {
 					Helper.printProcOut(p)
 					p.waitFor(10, TimeUnit.SECONDS)
 					dbMan.save()
+					Thread.sleep(sleep)
 				}
 			}
-			
-									
 		}
-		
-		
 	}
 
 
 	public static void main(String[] args) {
 		EnTSSAppender a = new EnTSSAppender()
-		//a.runTerms()
-		a.runExamples()
+		a.runTerms()
+		//a.runExamples()
 		println "Done"
 	}
 }
