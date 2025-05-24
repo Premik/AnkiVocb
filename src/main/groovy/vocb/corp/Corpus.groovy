183d572caaf2fc9715fdd6c496b3cbcbd8f803c9
Premik <premysl.srubar@post.cz>
2025-05-24 14:35:07 +0200

    Removed unused corpuses

--- src/main/groovy/vocb/corp/Corpus.groovy
+++ src/main/groovy/vocb/corp/Corpus.groovy
@@ -37,35 +37,7 @@ public class Corpus {
 		limitCounter++		
 		return true
 	}
-
-
-	//https://www.wordfrequency.info/free.asp
-	void importWordFreqCsvCorpus(Map parserArgs = [:], Reader reader) {
-		String dispersionCol="Dispersion"
-		Iterator csvLines = CsvParser.parseCsv(parserArgs, reader)
-		for (line in csvLines) {
-			if (!checkLimit()) break
-			String w = line."Word".toLowerCase().replace('\u00A0',' ').trim()
-
-			String f = line."Frequency"
-
-			//String f  = line."Dispersion"
-			if (!f || !w) {
-				continue
-			}
-			wordFreq[w] = new BigDecimal(f)
-		}
-		println "Imported ${wordFreq.size()} words from Csv"
-	}
-
-	void loadWordFreq() {
-		getClass().getResource('/wordFreq.csv').withReader(StandardCharsets.UTF_8.toString()) {
-			importWordFreqCsvCorpus([:], it)
-		}
-	}
-
-
-
+	
 	//https://www.kaggle.com/datasets/rtatman/english-word-frequency
 	void importKaggleEnglishWordFreqCsv(Reader reader, boolean normalize=true) {
 		Iterator csvLines = CsvParser.parseCsv([:], reader)
@@ -94,99 +66,6 @@ public class Corpus {
 			importKaggleEnglishWordFreqCsv(it)
 		}
 	}
-
-	//https://github.com/en-wl/wordlist/tree/master/alt12dicts
-	void import12WordFreq(InputStream input) {
-		//2+2+3frq.txt
-		Pattern blockDeli= ~/-+\s*(\d+)\s*-+/
-		Pattern wordPatter = ~/^\w+/
-		int groupIndex = 0
-
-		input.eachLine(utf8) { String line ->
-
-			def blocMatcher = line =~  blockDeli
-			if (blocMatcher) {
-				groupIndex = blocMatcher[0][1] as int
-				return
-			}
-			def wordMatcher = line =~  wordPatter
-			if (!wordMatcher) {
-				return
-			} //Ignore lemming
-			String w = wordMatcher[0]
-			//println w
-
-			if (checkLimit()) {
-				wordFreq[w.toLowerCase()] = new BigDecimal(1-groupIndex/120d) //Magic factor to get similar Disp as wordfreq
-				if (limit != 0) {
-					limit--
-				}
-			}
-			//println "$groupIndex: $w "
-		}
-		println "Imported ${wordFreq.size()} words. Groups: $groupIndex"
-	}
-
-	void import12Phrases(InputStream input, int limit = 0) {
-		//6of12.txt
-		Pattern phrasePatter = ~/^\w\w+\s\w\w+/
-
-		input.eachLine(utf8) { String line ->
-
-			def phraseMatcher = line =~  phrasePatter
-			if (phraseMatcher) {
-				phrases+= phraseMatcher[0].toLowerCase()
-				return
-			}
-		}
-
-		println "Imported ${phrases.size()} phrases"
-	}
-	//https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/PG/2006/04/1-10000
-
-	void importWikiWordList(InputStream input, int limit = 0) {
-		assert input != null : "No input provided"
-		//SAXParser parser = new SAXParser()
-		//def parser =new org.ccil.cowan.tagsoup.Parser()
-		//def page = new XmlSlurper(parser).parse(input)
-
-		def page = new XmlSlurper().parse(input)
-		GPathResult content = page.body.'**'.find {it.@id == 'bodyContent'}
-		content.'**'.each { GPathResult table ->
-			if (table.name() != 'table') {
-				return
-			}
-
-			table.tbody.tr.eachWithIndex { GPathResult row, int i ->
-				if (i == 0) {
-					return
-				}
-				String w = row.td[1].a.toString().trim().toLowerCase()
-				String r = row.td[2].toString().trim().toLowerCase()
-
-				wordFreq[w.toLowerCase()] = new BigDecimal(r)
-			}
-		}
-		println "Imported ${wordFreq.size()} words for wiki pages"
-	}
-
-
-
-	void loadWiki() {
-		importWikiWordList(cfgHelper.resolveRes("Wiktionary_Frequency lists_PG_2006_04_1-10000 - Wiktionary"))
-		importWikiWordList(cfgHelper.resolveRes("Wiktionary Frequency lists_PG_2006_04_10001-20000 - Wiktionary"))
-		importWikiWordList(cfgHelper.resolveRes("Wiktionary Frequency lists_PG_2006_04_20001-30000 - Wiktionary"))
-		importWikiWordList(cfgHelper.resolveRes("Wiktionary Frequency lists_PG_2006_04_30001-40000 - Wiktionary"))
-	}
-
-	void load12Dicts() {
-		import12Phrases(getClass().getResource('/6of12.txt').openStream())
-		import12WordFreq(getClass().getResource('/2+2+3frq.txt').openStream())
-	}
-
-
-
-
 
 	void averageCommonWordsFrom(Corpus c) {
 		assert c?.wordFreq : "No corpus or blank freq. words provided"
