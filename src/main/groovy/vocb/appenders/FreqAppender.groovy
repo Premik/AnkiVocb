e447fb44fc44bb3b8ff8b1957d7a66d1b627f49f
Premik <premysl.srubar@post.cz>
2025-05-24 09:45:15 +0200

    Freq appender

---
+++ src/main/groovy/vocb/appenders/FreqAppender.groovy
@@ -0,0 +1,37 @@
+package vocb.appenders
+
+import vocb.Helper
+import vocb.corp.Corpus
+import vocb.corp.WordNormalizer
+import vocb.data.Concept
+import vocb.data.Manager
+import vocb.data.Term
+
+public class FreqAppender {
+
+	@Lazy Corpus corp = Corpus.buildDef()
+
+
+	Manager dbMan = new Manager()
+
+	void run() {
+		dbMan.load()
+
+
+		dbMan.withTermsByLang("en") {Concept c, Term t->
+			if (!c.freq) {
+				c.freq = corp.wordFreq[t.term]				
+			} 
+		}
+		dbMan.save()
+		
+	}
+
+
+	public static void main(String[] args) {
+		FreqAppender a = new FreqAppender()
+		a.run()
+
+		println "Done"
+	}
+}
