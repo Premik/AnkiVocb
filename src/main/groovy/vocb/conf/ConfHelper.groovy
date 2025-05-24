79685d72c3719f9c2ccf43b1038af879c79a2f13
Premik <premysl.srubar@post.cz>
2025-05-24 13:24:40 +0200

    Conf refactoring, pack refactoring

--- src/main/groovy/vocb/conf/ConfHelper.groovy
+++ src/main/groovy/vocb/conf/ConfHelper.groovy
@@ -3,9 +3,19 @@ package vocb.conf
 import static java.lang.System.getProperty
 import static java.lang.System.getenv
 
+import groovy.transform.Synchronized
+import groovy.util.logging.Log4j2
+
+@Log4j2
 class ConfHelper {
 
+	@Lazy
+	public static ConfHelper instance = new ConfHelper().tap {
+		loadDefault()
+	}
 
+	@Lazy  public static ConfigObject cfg = instance.config
+	
 	public final configRootFolder="Ankivocb"
 	public  final List<String> resExplicitExtensions = ['.conf', '.html']
 	public  final List<String> cpFolders = [
@@ -20,7 +30,7 @@ class ConfHelper {
 		'vocb/wiki',
 		'vocb/templates'
 	]
-	private  String windowsHomePath = "${getenv('HOMEDRIVE')}${getenv('HOMEPATH')}"
+	private static String windowsHomePath = "${getenv('HOMEDRIVE')}${getenv('HOMEPATH')}"
 
 
 	private  ConfigObject mergedCfg
@@ -28,7 +38,7 @@ class ConfHelper {
 	@Lazy  public  ConfigObject config = {
 		if (mergedCfg == null) {
 			mergedCfg = new ConfigObject()
-			loadAll()
+			loadDefault()
 		}
 		return mergedCfg
 	}()
@@ -84,17 +94,25 @@ class ConfHelper {
 	}
 
 	public   ConfigObject loadAndMergeConfig(String configName, Map binding = null) {
+		log.info("Loading $configName config")
 		ConfigObject c = loadConfig(configName, binding)
-		if (!c) return c
-		if (mergedCfg != null) mergedCfg.merge(c)
-		return c
+		if (!c) throw new IllegalArgumentException("Failed to load the '$configName' config.\n$confPathDetails")
+		if (mergedCfg != null) {
+			mergedCfg.merge(c)
+		} else {
+			mergedCfg = c
+		}
+
+		return mergedCfg
 	}
 
 	public InputStream resolveResExactName(String resName, File[] lookupPaths =lookupFolders + extraLookupFolders) {
 		if (!resName) return null
 		//First try to find a file. Take the first match
 		File file = lookupPaths.findResult { File pf->
-			File f =  new File(pf, resName)
+			File f
+			if (!pf) f = new File(resName) //Res name is (absolute) path on its own
+			else f = new File(pf, resName)
 			if (f.exists() && f.isFile()) return f
 			else return null
 		}
@@ -127,12 +145,12 @@ class ConfHelper {
 
 	public  InputStream resolveRes(String resName, File[] lookupPaths = lookupFolders + extraLookupFolders, Closure cb) {
 		(['']+ resExplicitExtensions).findResult {
-			cb("$resName$it", lookupPaths)
+			cb("$resName$it", [null, *lookupPaths] as File[] ) //Allow using path as resName
 		}
 	}
 
 
-	public  StringBuilder prettyPrintCfg(obj=config, int level = 0, StringBuilder sb = new StringBuilder()) {
+	public  StringBuilder toPrettyString(obj=config, int level = 0, StringBuilder sb = new StringBuilder()) {
 		//https://stackoverflow.com/questions/7898068/pretty-print-for-a-groovy-configobject
 		Closure indent = { lev -> sb.append("  " * lev) }
 		if(obj instanceof Map){
@@ -141,7 +159,7 @@ class ConfHelper {
 				if(name.contains('.')) return // skip keys like "a.b.c", which are redundant
 					indent(level+1).append(name)
 				(value instanceof Map) ? sb.append(" ") : sb.append(" = ")
-				prettyPrintCfg(value, level+1, sb)
+				toPrettyString(value, level+1, sb)
 				sb.append("\n")
 			}
 			indent(level).append("}")
@@ -150,7 +168,7 @@ class ConfHelper {
 			sb.append("[\n")
 			obj.each{ value ->
 				indent(level+1)
-				prettyPrintCfg(value, level+1, sb).append(",")
+				toPrettyString(value, level+1, sb).append(",")
 				sb.append("\n")
 			}
 			indent(level).append("]")
@@ -163,20 +181,30 @@ class ConfHelper {
 		}
 	}
 
+	public String getConfPathDetails() {
+		String cps = cpFolders.collect{"cp:$it"}.join(" ")
+		return "$lookupFoldersToConsider  $cps"
+	}
 
-	public  loadAll() {
-		assert loadAndMergeConfig("ankivocb-default") : "Failed to load the inbuilt default config"
+	public loadDefault() {
+		assert loadAndMergeConfig("ankivocb-default") :
+		"""\n
+              Failed to load the inbuilt 'default.conf' config. Paths considered: 
+              $confPathDetails""".stripIndent()
+		
 		ConfigObject c=  loadAndMergeConfig("ankivocb")
-		if (!c) System.print("Couldn't find the custom ankivocb.conf file. Using the default.\n")
+		if (!c) log.warn("Couldn't find the custom ankivocb.conf file. Using the default.\n")
 	}
 
 
 	public static void main(String[] args) {
 		new ConfHelper().with {
-			println lookupFoldersToConsider
-			println lookupFolders
+			println confPathDetails
+			loadDefault()
 			
-			println prettyPrintCfg()
+			
+			
+			println toPrettyString()
 		}
 		
 	}
