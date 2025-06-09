e19f1e3540518a5d556a4717cba927a51867cc38
Premik <premysl.srubar@post.cz>
2025-06-09 21:50:27 +0200

    tts back

--- src/main/groovy/vocb/appenders/CsTSSAppender.groovy
+++ src/main/groovy/vocb/appenders/CsTSSAppender.groovy
@@ -31,8 +31,8 @@ public class CsTSSAppender {
 		voices[voiceCounter%(voices.length)]
 	}
 
-	LocalTTS ttsCz = new LocalTTS()
-	//Object ttsCz = new AzureTTs()
+	//LocalTTS ttsCz = new LocalTTS()
+	Object ttsCz = new AzureTTs()
 	Manager dbMan = new Manager()
 	int limit = 10
 	WordNormalizer wn = new WordNormalizer()
